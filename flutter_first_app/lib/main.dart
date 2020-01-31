a03a37f70f4f7ab5054be5a582d71ddf7857832c
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-01-31 10:29:54 +0700

    Add scaffold

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('Hey brother'),
+  home: Scaffold(
+    appBar: AppBar(
+      title:Text('My first flutter app'),//propertice and value
+      centerTitle: true,
+    ),
+    body: Center(
+        child: Text('Hello world'),
+    ),
+
+    floatingActionButton: FloatingActionButton(
+      child:Text('click'),
+    ),
+
+  )
 
 ));
