076a6006b4298a7e443c74761b69b5148162f661
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-02-03 15:07:53 +0700

    add remote image, local image

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title:Text('My first flutter app'),//propertice and value
-      centerTitle: true,
-    ),
-    body: Center(
-        child: Text('Hello world'),
-    ),
-
-    floatingActionButton: FloatingActionButton(
-      child:Text('click'),
-    ),
-
-  )
-
+  home: Home(),
 ));
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('My first flutter app'),//propertice and value
+        centerTitle: true,
+        backgroundColor: Colors.red[700],//press ctrl+q to view more color
+      ),
+      body: Center(
+        child: Image(
+          //Network image
+          //image: NetworkImage('https://expertvagabond.com/wp-content/uploads/isle-of-skye-guide-900x600.jpg')
+
+          //Local image
+          image: AssetImage('assets/medicine_budda.jpg')
+
+        )
+      ),
+
+      //every widget is class
+      floatingActionButton: FloatingActionButton(//instance of class
+        child:Text('click here'),
+        onPressed: (){
+
+        },
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.red[200],
+      ),
+
+    );
+
+  }
+}
