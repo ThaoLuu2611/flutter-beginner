67e0034009eb4e1376a5268889fa69826f1c999a
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-02-25 15:03:59 +0700

    navigate screen

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -1,79 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+//import 'package:table_calendar/table_calendar.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-));
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('welcome to flutter'),
-      ),
-      body: Center(
-        child: RandomeWord(),
-      )
+    return MaterialApp(
+      title: 'Flutter',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+      home: RandomWords()
     );
   }
 }
 
-class RandomWordState extends State<RandomeWord>{
-  final List<WordPair> _suggestion = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 13);
-  final Set<WordPair> saved = Set<WordPair>();
-  Widget _suggestionList(){
+
+class RandonWordsState extends State<RandomWords>{
+  final WordPair word = WordPair.random();
+  List<WordPair> _list = <WordPair>[];
+  Set<WordPair> set = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _suggestion(){
     return ListView.builder(
-        itemBuilder: (BuildContext _context, int i){
-          if(i.isOdd){
-            return Divider();
-          }
-          final index = i ~/ 2;
-          if(index >= _suggestion.length)
-            _suggestion.addAll(generateWordPairs().take(110));
-          return _buildRow(_suggestion[index]);
-        }
-
-        );
-
+        itemBuilder: (BuildContext context, int i){
+            if(i.isOdd)
+              return Divider();
+            int index = i~/2;
+            if(index >= _list.length){
+              _list.addAll(generateWordPairs().take(10));
+            }
+            return(_row(_list[index]));
+        });
   }
 
-  Widget _buildRow(WordPair pair){
-    final bool alreadySaved = saved.contains(pair);
+  Widget _row(WordPair pair) {
+    final isSaved = set.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
+      title: Text(pair.asPascalCase),
+      contentPadding: EdgeInsets.all(10),
       trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved? Colors.red : Colors.green,
+        isSaved ? Icons.favorite : Icons.favorite_border,
+        color: isSaved ? Colors.red : Colors.green,
       ),
       onTap: (){
         setState(() {
-          if(alreadySaved){
-            saved.remove(pair);
-          } else
-            saved.add(pair);
+          if(isSaved)
+            set.remove(pair);
+          else {
+            set.add(pair);
+        }
         });
       },
+
     );
   }
-  final WordPair wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
-    //return Text(wordPair.asPascalCase);
     return Scaffold(
       appBar: AppBar(
-        title: Text('My list view'),
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
-      body: _suggestionList()
+      body: _suggestion()
+    );
 
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = set.map(
+              (WordPair pair){
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  )
+                );
+              }
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved suggestion'),
+            ),
+            body: ListView(children: divided),
+          );
+
+        }
+      )
     );
   }
-}
 
-class RandomeWord extends StatefulWidget{
-  RandomWordState createState() => RandomWordState();
+}
+class RandomWords extends StatefulWidget{
+  @override
+  RandonWordsState createState() => RandonWordsState();
 }
