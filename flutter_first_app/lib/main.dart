345ea6fcbac28c19ad27a1f6f82f61354a03ac4d
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-02-27 16:15:10 +0700

    add list

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -4,171 +4,31 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyCard(),
+  home: MyList(),
 ));
 
-class MyCard extends StatefulWidget {
+class MyList extends StatefulWidget {
   @override
-  _MyCardState createState() => _MyCardState();
+  _MyListState createState() => _MyListState();
 }
 
-class _MyCardState extends State<MyCard> {
-  int age = 2;
-  String name = "hihi";
+class _MyListState extends State<MyList> {
+  List<String> quotes = [
+    'How to build layouts using F','lutter’s layout ','mechanism. Once you’ve learned ','basic principles, you’ll',' build the layout',' for a sample screenshot.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('fluuter'),
+        backgroundColor: Colors.red[200],
+        title: Text('Awesome lists'),
+        centerTitle: true,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Text(
-              '$name',
-            style: TextStyle(
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          SizedBox(height: 30),
-          Text(
-              '$age',
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.add),
-            color: Colors.green,
-            focusColor: Colors.black26,
-            onPressed: (){
-              setState(() {
-                age +=1;
-                if(age == 20)
-                  age = 0;
-                name = "name "+ age.toString();
-              });
-            },
-          )
-
-        ],
-      )
-    );
-  }
-}
-
-/*
-
-class MyCard extends StatefulWidget {
-  @override
-  _MyCardState createState() => _MyCardState();
-}
-
-class _MyCardState extends State<MyCard> {
-  int level = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('My ID card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState(() {
-            level +=1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[600],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 39, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-           Center(
-             child: CircleAvatar(
-               backgroundImage: AssetImage('assets/icon4.png'),
-               radius: 30.0,
-             )
-           ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/icon2.png'),
-                radius: 40.0,
-              )
-            ),
-            Divider(height: 30),
-
-            Text(
-              'NAME',
-              style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 1.0
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Thao',
-              style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 1.0,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'NAME',
-              style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 1.0
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              '$level',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 1.0,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 20.0),
-                Text(
-                  'Leee hll',
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 10.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            )
-          ]
-        ),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((q) => Text(q)).toList()
       ),
     );
   }
 }
-
-
-
-*/
