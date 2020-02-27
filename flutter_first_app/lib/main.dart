bad8a83131b06e4b93ae200401a969fa7f7c57b9
thaoltp11 <thaoltp11@fsoft.com.vn>
2020-02-27 21:49:01 +0700

    add list book

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'quote.dart';
 
 void main() => runApp(MaterialApp(
-  home: MyList(),
-));
+      home: MyList(),
+    ));
 
 class MyList extends StatefulWidget {
   @override
@@ -14,29 +14,48 @@ class MyList extends StatefulWidget {
 }
 
 class _MyListState extends State<MyList> {
-  List<Quote> quotes = [
-    Quote(author: 'Lee', text: 'Call of the wind'),
-    Quote(author: 'Lee', text: 'Call of the wind'),
-    Quote(author: 'Lee', text: 'Call of the wind'),
-    Quote(author: 'Lee', text: 'Call of the wind'),
-    Quote(author: 'Lee', text: 'Call of the wind'),
-
-
+  List<Quote> books = [
+    Quote(text: 'Call of the Wind', author: 'Lee'),
+    Quote(text: 'Call of the Wind', author: 'Lee'),
+    Quote(text: 'Call of the Wind', author: 'Lee'),
+    Quote(text: 'Call of the Wind', author: 'Lee'),
   ];
 
+  Widget bookCard(quote) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      child: Container(
+        padding: EdgeInsets.all(30),
+        color: Colors.amberAccent,
+        child: Column(
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              quote.author,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[200],
-        title: Text('Awesome lists'),
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: quotes.map((q) => Text('${q.author} - ${q.text}')).toList()
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Awesome Flutter'),
+        ),
+        backgroundColor: Colors.grey[200],
+        body: ListView(
+          children: books.map((book) => bookCard(book)).toList(),
+        ));
   }
 }
